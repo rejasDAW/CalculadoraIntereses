43f8c1f15da3dededa1372282912c71d43c75a01
RejasDAW <rejas.indus@gmail.com>
2026-01-26 00:49:30 +0100

    feat: Implement link cloaking/redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://calculadoraintereses.com',
+  redirects: {
+    '/ir/imagin': 'https://imagin.pwlnk.io/uj$&JA4Qc',
+    '/ir/revolut': 'https://revolut.com/referral/?referral-code=rejas99!JAN1-26-AR-CL1H2-CRY&geo-redirect',
+    '/ir/traderepublic': 'https://traderepublic.com', // Placeholder
+  },
   integrations: [tailwind()],
   vite: {
     server: {
